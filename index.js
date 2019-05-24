171da3c01bb9282caf1043a1fa75d75a581b06e6
ywettai-dev <ywettai.dev@gmail.com>
2019-05-24 16:09:44 +0630

    Add crypto & fiat respond with current date time stamp

--- index.js
+++ index.js
@@ -3,17 +3,43 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
 const bodyParser = require('body-parser');
+const request = require('request');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 //Home
 app.get('/', (req, res) => res.sendFile(__dirname + `/index.html`));
 
-app.post('/', function(req, res) {
-    console.log(req.body.crypto);
+app.post('/', function (req, res) {
+
+    var crypto = req.body.crypto;
+    var fiat = req.body.fiat;
+    var cryptoFiat = crypto+fiat;
+    var baseURL = '';
+
+    if(crypto === "BTC"){
+        baseURL = 'https://apiv2.bitcoinaverage.com/indices/global/ticker/';
+    }else{
+        baseURL = 'https://apiv2.bitcoinaverage.com/indices/local/ticker/';
+    }
+
+    var finalURL = baseURL + cryptoFiat;
+
+    request(finalURL, function(error, response, body){
+        
+        var data = JSON.parse(body);
+        var price = data.last;
+        var currentDate = data.display_timestamp;
+        
+        res.write(`<p>Current Date is ${currentDate}</p>`);
+        res.write(`<p>The latest price of ${crypto} in ${fiat}: ${price}</p>`);
+
+        res.send();
+    });
+
 });
 
 app.listen(port, () => console.log(`Bitcoin Ticker starts on ${port}`));
-
