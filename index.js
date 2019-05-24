01ca6c63c0a13db8b3d2daf7a85a0b095c42c056
ywettai-dev <ywettai.dev@gmail.com>
2019-05-24 18:08:43 +0630

    Create crypto converter from cyrpto to fiat.

--- index.js
+++ index.js
@@ -17,28 +17,56 @@ app.post('/', function (req, res) {
 
     var crypto = req.body.crypto;
     var fiat = req.body.fiat;
-    var cryptoFiat = crypto+fiat;
+    var amount = req.body.amount;
+
+    //Crypto latest price
+    /*var cryptoFiat = crypto + fiat;
     var baseURL = '';
 
-    if(crypto === "BTC"){
+    if (crypto === "BTC") {
         baseURL = 'https://apiv2.bitcoinaverage.com/indices/global/ticker/';
-    }else{
+    } else {
         baseURL = 'https://apiv2.bitcoinaverage.com/indices/local/ticker/';
     }
 
     var finalURL = baseURL + cryptoFiat;
 
-    request(finalURL, function(error, response, body){
-        
+    request(finalURL, function (error, response, body) {
+
         var data = JSON.parse(body);
         var price = data.last;
         var currentDate = data.display_timestamp;
-        
+
         res.write(`<p>Current Date is ${currentDate}</p>`);
         res.write(`<p>The latest price of ${crypto} in ${fiat}: ${price}</p>`);
 
         res.send();
+    });*/
+
+    //Crypto Converter from crypto to fiat
+    var options = {
+        url: 'https://apiv2.bitcoinaverage.com/convert/global',
+        method: 'GET',
+        qs: {
+            from: crypto,
+            to: fiat,
+            amount: amount
+        }
+    };
+
+    request(options, function(error, response, body){
+
+        var data = JSON.parse(body);
+        var price = data.price;
+        var currentDate = data.time;
+
+        res.write(`<p>Current Date is ${currentDate}.</p>`);
+        res.write(`<p>${amount}${crypto} is currently worth ${price}${fiat}.</p>`);
+
+        res.send();
+
     });
+
 
 });
 
